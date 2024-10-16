0ad5db73c5f8f1689f05b6d7930736658a558ac6
t-site <sktruntime@hotmail.co.jp>
2024-10-16 20:40:30 +0900

    Initial commit

---
+++ README.txt
@@ -0,0 +1,22 @@
+LICENSE
+
+License of this program is "unprovability".
+this is not a program.
+so this is a true "BLOB"
+
+NOTICE IN ENGLISH
+
+DO NOT USE FOR "WORKS" "VOLUNTEERS" and "COMFORT"
+DO NOT USE IN NORMAL
+FOR ELIMINATE ANOMALY FUNCTION of UNIX MACHINES
+
+ABOUT THIS BLOB
+
+this is unprovability program.
+for eliminate anomaly function of UNIX machines.
+no needs i18n support.
+functions of this is unprovability , you catch gain or not.
+
+PORTIONS
+
+mostly DeepL translation from japanese.
---
+++ rand.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+MEMDIR="./mem/"
+MEMF=$( basename "$1" )
+MEM="${MEMDIR}${MEMF/txt/mem}"
+lines=$( wc -l "$1" )
+lines="${lines% *}"
+ME=$(< "${MEM}" )
+M=$(( "${ME% *}" + "${ME#* }" ))
+M=$(( "${M}" / 2 ))
+RWINDOW=$(( "${2}" / "${lines}" ))
+RANGE=$(( "${RANDOM}" / "${RWINDOW}" ))
+if [ "${RANGE}" -le "1" ]
+then
+	RANGE=2
+fi
+RR=${RANDOM}
+PM=$(( ${RR} % 2 ))
+if [ "${PM}" -eq 0 ]
+then
+	RR="-$RR"
+fi
+LIN=$(( ${RR} % ${RANGE} ))
+OUT=$(( "${M}" + "${LIN}" ))
+
+while [ "${OUT}" -gt "${lines}" ]
+do
+	OUT=$(( "${OUT}" - "${lines}" ))
+done
+while [ "${OUT}" -lt 1 ]
+do
+	OUT=$(( "${OUT}" + "${lines}" ))
+done
+
+echo "${OUT}" "${M% *}" > "${MEM}" 
+echo "${OUT}"
