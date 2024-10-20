b033a8396806e0aa61a25a9ef633d4ac04f39f5d
t-site <sktruntime@hotmail.co.jp>
2024-10-21 08:00:00 +0900

    Revert "Revert "for *******bility""
    This reverts commit 668f15bb200eb5f91d5ee7faff5661bfe5f03340.
    
    Changes to be committed:
    	modified:   rand.sh

--- rand.sh
+++ rand.sh
@@ -5,30 +5,32 @@ MEM="${MEMDIR}${MEMF/txt/mem}"
 lines=$( wc -l "$1" )
 lines="${lines% *}"
 ME=$(< "${MEM}" )
-M=$(( "${ME% *}" + "${ME#* }" ))
-M=$(( "${M}" / 2 ))
+M1="${ME#* }"
+M2="${ME% *}"
+M=$(( M1 + M2 ))
+M=$(( M / 2 ))
 RWINDOW=3
-RANGE=$(( "${RANDOM}" / "${RWINDOW}" ))
+RANGE=$(( RANDOM / RWINDOW ))
 if [ "${RANGE}" -le "1" ]
 then
 	RANGE=2
 fi
 RR=${RANDOM}
-PM=$(( ${RR} % 2 ))
+PM=$(( RR % 2 ))
 if [ "${PM}" -eq 0 ]
 then
 	RR="-$RR"
 fi
-LIN=$(( ${RR} % ${RANGE} ))
-OUT=$(( "${M}" + "${LIN}" ))
+LIN=$(( RR % RANGE ))
+OUT=$(( M + LIN ))
 
 while [ "${OUT}" -gt "${lines}" ]
 do
-	OUT=$(( "${OUT}" - "${lines}" ))
+	OUT=$(( OUT - lines ))
 done
 while [ "${OUT}" -lt 1 ]
 do
-	OUT=$(( "${OUT}" + "${lines}" ))
+	OUT=$(( OUT + lines ))
 done
 
 echo "${OUT}" "${M% *}" > "${MEM}" 
