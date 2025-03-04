a1be910e2282106fea29ca5ef660c61fc4b04453
t-site <sktruntime@hotmail.co.jp>
2025-03-04 21:41:55 +0000

    more ****rate

--- rand.sh
+++ rand.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
-MEMDIR="./mem/"
-MEMF=$( basename "$1" )
-MEM="${MEMDIR}${MEMF/txt/mem}"
 lines=$( wc -l "$1" )
 lines="${lines% *}"
-ME=$(< "${MEM}" )
-M1="${ME#* }"
-M2="${ME% *}"
-M=$(( M1 + M2 ))
-M=$(( M / 2 ))
-RWINDOW=3
-RANGE=$(( RANDOM / RWINDOW ))
-if [ "${RANGE}" -le "1" ]
-then
-	RANGE=2
-fi
-RR=${RANDOM}
-PM=$(( RR % 2 ))
-if [ "${PM}" -eq 0 ]
-then
-	RR="-$RR"
-fi
-LIN=$(( RR % RANGE ))
-OUT=$(( M + LIN ))
+
+OUT=$(( RANDOM % ( lines * 3 ) ))
+OUT=$(( OUT - lines ))
 
 while [ "${OUT}" -gt "${lines}" ]
 do
@@ -33,5 +14,4 @@ do
 	OUT=$(( OUT + lines ))
 done
 
-echo "${OUT}" "${M% *}" > "${MEM}" 
 echo "${OUT}"
