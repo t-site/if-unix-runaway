6026958aeaadd357370e08e76bc4aaa0037f9967
t-site <sktruntime@hotmail.co.jp>
2025-04-07 23:20:10 +0000

    Add files via upload

--- iu3.sh
+++ iu3.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+while read
+do
+	lines=$( cat $1 | wc -l )
 
-LINE=$(. ./rand.sh em.txt )
-head -"${LINE}" em.txt | tail -1
+[ -f $1.mem ] || echo 3 > $1.mem
+
+MEM=$(< $1.mem )
+R=$(( RANDOM % lines ))
+R=$(( R / 16 ))
+T=$(( R % 2 ))
+[ $T -eq 0 ] && R=-$R
+L=$(( MEM + R ))
+
+[ $L -gt $lines ] && L=$(( L - lines ))
+[ $L -lt 1 ] && L=$(( L + lines ))
+
+head -$L $1 | tail -1
+done
